6286ca07a6a29337b8c08d5b1f1790bee1d8bd4f
jx13xx <jeenxavier23@gmail.com>
2022-12-30 09:17:03 +0400

    chore: init files

---
+++ src/main/java/com/rak/teslaprototype/TeslaPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.rak.teslaprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeslaPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeslaPrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rak/teslaprototype/TeslaPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rak.teslaprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslaPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
